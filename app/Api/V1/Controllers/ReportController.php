f946d189aacf2c5a60c44c8261c683d25f19b3b0
Unknown <angger.dayu@yahoo.com>
2018-10-22 16:00:14 +0700

    update report

--- app/Api/V1/Controllers/ReportController.php
+++ app/Api/V1/Controllers/ReportController.php
@@ -12,11 +12,6 @@ use Auth;
 
 class ReportController extends BaseController
 {
-    public function __construct(GlobalCrudRepo $globalCrudRepo)
-    {
-        // $this->globalCrudRepo = $globalCrudRepo;
-        // $this->globalCrudRepo->setModel(new MwMappingHistory());
-    }
 
     public function index()
     {
@@ -37,16 +32,99 @@ class ReportController extends BaseController
         // $this->filters($request);
         $data = RptDriverScoring::raw(function($collection) use ($request)
         {  
+            $search['$match']['alert_status'] = ['$in' => ['Overspeed','Signal Jamming','Out Of Zone']];
+            if($request->has('license_plate') && !empty($request->license_plate)){
+              $search['$match']['license_plate'] = $request->license_plate;
+            }
+           
+            if($request->has('startDate') || $request->has('endDate')){
+               $created_at = [];
+               $gte = $request->has('startDate') ? $request->startDate : '';
+               $lte = $request->has('endDate') ? $request->endDate : '';
+               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte)*1000);
+               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte)*1000);
+               if(!empty($created_at)) $search['$match']['created_at'] = $created_at;
+            }
             $query = [
             [
                 '$project' => array(
-                    'created_at'     => '$created_at',
-                    'driver'         => '$driver_name',
                     'license_plate'  => '$license_plate',
+                    'driver'         => '$driver_name',
+                    'over_speed'     => [
+                        '$cond' => [
+                            'if'   => [ '$eq' => [ '$alert_status', 'Overspeed' ]],
+                            'then' => '$score',
+                            'else' =>  0
+                        ]
+                    ],
+                    'harsh_acceleration' => [ //confirm
+                        '$cond' => [
+                            'if'   => [ '$eq' => [ '$alert_status', 'Overspeed' ]],
+                            'then' => '$score',
+                            'else' =>  0
+                        ]
+                    ],
+                    'harsh_braking' => [ //confirm
+                        '$cond' => [
+                            'if'   => [ '$eq' => [ '$alert_status', 'Overspeed' ]],
+                            'then' => '$score',
+                            'else' =>  0
+                        ]
+                    ],
+                    'harsh_cornering' => [ //confirm
+                        '$cond' => [
+                            'if'   => [ '$eq' => [ '$alert_status', 'Overspeed' ]],
+                            'then' => '$score',
+                            'else' =>  0
+                        ]
+                    ],
+                    'main_power_cut' => [
+                        '$cond' => [
+                            'if'   => [ '$eq' => [ '$alert_status', 'Main Power Remove' ]],
+                            'then' => '$score',
+                            'else' =>  0
+                        ]
+                    ],
+                    'signal_jamming' => [
+                        '$cond' => [
+                            'if'   => [ '$eq' => [ '$alert_status', 'Signal Jamming' ]],
+                            'then' => '$score',
+                            'else' =>  0
+                        ]
+                    ],
+                    'bump_detection' => [
+                        '$cond' => [
+                            'if'   => [ '$eq' => [ '$alert_status', 'Overspeed' ]],
+                            'then' => '$score',
+                            'else' =>  0
+                        ]
+                    ],
+                    'out_of_zone' => [ //confirm
+                        '$cond' => [
+                            'if'   => [ '$eq' => [ '$alert_status', 'Out Of Zone' ]],
+                            'then' => '$score',
+                            'else' =>  0
+                        ]
+                    ],   
+                    'immobilizer' => [ //confirm
+                        '$cond' => [
+                            'if'   => [ '$eq' => [ '$alert_status', 'Overspeed' ]],
+                            'then' => '$score',
+                            'else' =>  0
+                        ]
+                    ],       
+                    'un_immobilizer' => [ //confirm
+                        '$cond' => [
+                            'if'   => [ '$eq' => [ '$alert_status', 'Overspeed' ]],
+                            'then' => '$score',
+                            'else' =>  0
+                        ]
+                    ], 
+                    'alert_status'   => '$alert_status',
                     'total'          => '$score',
                 )
             ]];
-            return $collection->aggregate($query);
+            return $collection->aggregate(array_merge([$search], $query));
         });
         return $this->makeResponse(200, 1, null, $data);
     }
@@ -64,60 +142,50 @@ class ReportController extends BaseController
                $created_at = [];
                $gte = $request->has('startDate') ? $request->startDate : '';
                $lte = $request->has('endDate') ? $request->endDate : '';
-               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte . " 00:00:00")*1000);
-               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte . " 24:00:00")*1000);
+               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte)*1000);
+               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte)*1000);
                if(!empty($created_at)) $search['$match']['created_at'] = $created_at;
             }
+            $duration = Carbon::parse($request->startDate);
+            $duration = (int) $duration->diffInMinutes($request->endDate);
             $query = [
-                 [
-                    '$project' => array(
-                        '_id'               => 1,
-                        'license_plate'     => 1,
-                        'vehicle_number'    => 1,
-                        'machine_number'    => 1,
-                        'speed'             => 1,
-                        'duration_out_zone' => 1,
-                        'fuel_consumed'     => 1
-                    )
-                ],
                 [
                     '$group' => array(
-                        '_id' => [
-                            'license_plate'  => '$license_plate',
-                            'vehicle_number' => '$vehicle_number',
-                            'machine_number' => '$machine_number',
-                        ],
-                       
-                        'total_data'  => ['$sum' => 1],
-                        'park_time'   => ['$sum' => '$park_time'],
-                        'moving_time' => ['$sum' => '$moving_time'],
-                        'idle_time'   => ['$sum' => '$idle_time'],
-                        'engine_on_time' => ['$sum' => '$engine_on_time'],
-                        'total_mileage'  => ['$sum' => '$total_odometer'],
-                        'speed' => ['$sum' => '$speed'],
+                        '_id' => '$imei',
+                        'license_plate'  => ['$first' => '$license_plate'],
+                        'vehicle_number' => ['$first' => '$vehicle_number'],
+                        'machine_number' => ['$first' => '$machine_number'],
+                        'total_data'     => ['$sum'   => 1],
+                        'park_time'      => ['$sum'   => '$park_time'],
+                        'moving_time'    => ['$sum'   => '$moving_time'],
+                        'idle_time'      => ['$sum'   => '$idle_time'],
+                        'engine_on_time' => ['$sum'   => '$engine_on_time'],
+                        'total_mileage'  => ['$sum'   => '$total_odometer'],
+                        'speed'          => ['$sum'   => '$speed'],
+                        'fuel'           => ['$sum'   => 'fuel_consumed'],
                         'duration_out_zone' => ['$sum' => '$duration_out_zone'],
-                        'fuel' => ['$sum' => 'fuel_consumed'],
                     )
                 ]
                 ,[
                     '$project' => array(
-                        'total_data'  => '$total_data',
-                        'park_time'   => '$park_time',
-                        'moving_time' => '$moving_time',
-                        'idle_time'   => '$idle_time',
-                        'fuel'        => '$fuel',
+                        '_id'            => 0,
+                        'license_plate'  => '$license_plate',
+                        'vin'            => '$vehicle_number',
+                        'machine_number' => '$machine_number',
+                        'total_data'     => '$total_data',
+                        'park_time'      => '$park_time',
+                        'moving_time'    => '$moving_time',
+                        'idle_time'      => '$idle_time',
+                        'fuel'           => '$fuel',
                         'engine_on_time' => '$engine_on_time',
                         'total_mileage'  => '$total_mileage',
                         'average_speed'  => [
                             '$divide' => [ '$speed', '$total_data']
                         ],
-                        // 'rasio_engine_on' => [
-                        //     '$multiply' => [[
-                        //         '$divide' => [100, '$engine_on_time']
-                        //     ], 100]
-                        // ],
-                        'duration_out_zone' => '$duration_out_zone',
-                        
+                        'rasio_engine_on' => [
+                            '$divide' => ['$engine_on_time', $duration]
+                        ],
+                        'duration_out_zone' => '$duration_out_zone'
                     )
                 ],
             ];
@@ -139,8 +207,8 @@ class ReportController extends BaseController
                $created_at = [];
                $gte = $request->has('startDate') ? $request->startDate : '';
                $lte = $request->has('endDate') ? $request->endDate : '';
-               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte . " 00:00:00")*1000);
-               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte . " 24:00:00")*1000);
+               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte)*1000);
+               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte)*1000);
                if(!empty($created_at)) $search['$match']['created_at'] = $created_at;
             }
             
@@ -186,8 +254,8 @@ class ReportController extends BaseController
                $created_at = [];
                $gte = $request->has('startDate') ? $request->startDate : '';
                $lte = $request->has('endDate') ? $request->endDate : '';
-               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte . " 00:00:00")*1000);
-               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte . " 24:00:00")*1000);
+               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte)*1000);
+               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte)*1000);
                if(!empty($created_at)) $search['$match']['created_at'] = $created_at;
             }
             
@@ -223,7 +291,13 @@ class ReportController extends BaseController
                                             'else' => 'Off'
                                         ]
                     ],
-                     // sleepmode (deep sleep)
+                    'sleep_mode'           => [
+                                        '$cond' => [
+                                            'if'   => [ '$eq' => [ '$deep_sleep', 0 ]],
+                                            'then' => 'Off',
+                                            'else' => 'On'
+                                        ]
+                    ],
                     'satellite'      => '$satellite',
                     'accu'           => '$internal_battery_voltage',
                     'gsm_signal'     => '$gsm_signal_level',
@@ -253,24 +327,38 @@ class ReportController extends BaseController
                $created_at = [];
                $gte = $request->has('startDate') ? $request->startDate : '';
                $lte = $request->has('endDate') ? $request->endDate : '';
-               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte . " 00:00:00")*1000);
-               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte . " 24:00:00")*1000);
+               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte)*1000);
+               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte)*1000);
                if(!empty($created_at)) $search['$match']['created_at'] = $created_at;
             }
             
             $query = [
                 [
+                '$group' => array(
+                        '_id' => '$imei',
+                        'license_plate'  => ['$first' => '$license_plate'],
+                        'vehicle_number' => ['$first' => '$vehicle_number'],
+                        'machine_number' => ['$first' => '$machine_number'],
+                        'driver_name'    => ['$first' => '$driver_name'],
+                        'start_address'  => ['$first' => '$last_location'],
+                        'end_address'    => ['$last'  => '$last_location'],
+                        'km_start'       => ['$first' => '$total_odometer'],
+                        'km_end'         => ['$last'  => '$total_odometer'],
+                    )
+                ],
+                [
                     '$project' => array(
+                        '_id'  => 0,
                         'license_plate'  => '$license_plate',
                         'driver'         => '$driver_name',
                         'vin'            => '$vehicle_number',
                         'machine_number' => '$machine_number',
-                        'start_address'  => '$last_location',
-                        'end_address'    => '$last_location',
-                        'km_start'       => '$total_odometer',
-                        'km_end'         => '$total_odometer',
-                        'km_driven'      => '$total_odometer',
-                        'total_odometer' => '$total_odometer',
+                        'start_address'  => '$start_address',
+                        'end_address'    => '$end_address',
+                        'km_start'       => '$km_start',
+                        'km_end'         => '$km_end',
+                        'km_driven'      => ['$subtract' => ['$km_end', '$km_start']],
+                        'total_odometer' => '$km_end',
                     )
                 ],
                 [
@@ -297,8 +385,8 @@ class ReportController extends BaseController
                $created_at = [];
                $gte = $request->has('startDate') ? $request->startDate : '';
                $lte = $request->has('endDate') ? $request->endDate : '';
-               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte . " 00:00:00")*1000);
-               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte . " 24:00:00")*1000);
+               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte)*1000);
+               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte)*1000);
                if(!empty($created_at)) $search['$match']['created_at'] = $created_at;
             }
             
@@ -347,8 +435,8 @@ class ReportController extends BaseController
                $created_at = [];
                $gte = $request->has('startDate') ? $request->startDate : '';
                $lte = $request->has('endDate') ? $request->endDate : '';
-               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte . " 00:00:00")*1000);
-               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte . " 24:00:00")*1000);
+               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte)*1000);
+               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte)*1000);
                if(!empty($created_at)) $search['$match']['created_at'] = $created_at;
             }
             
@@ -389,8 +477,8 @@ class ReportController extends BaseController
                $created_at = [];
                $gte = $request->has('startDate') ? $request->startDate : '';
                $lte = $request->has('endDate') ? $request->endDate : '';
-               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte . " 00:00:00")*1000);
-               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte . " 24:00:00")*1000);
+               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte)*1000);
+               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte)*1000);
                if(!empty($created_at)) $search['$match']['created_at'] = $created_at;
             }
             
@@ -429,8 +517,8 @@ class ReportController extends BaseController
                $created_at = [];
                $gte = $request->has('startDate') ? $request->startDate : '';
                $lte = $request->has('endDate') ? $request->endDate : '';
-               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte . " 00:00:00")*1000);
-               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte . " 24:00:00")*1000);
+               if(!empty($gte)) $created_at['$gte'] = new \MongoDB\BSON\UTCDatetime(strtotime($gte)*1000);
+               if(!empty($lte)) $created_at['$lte'] = new \MongoDB\BSON\UTCDatetime(strtotime($lte)*1000);
                if(!empty($created_at)) $search['$match']['created_at'] = $created_at;
             }
             
